38052f3b1cda699a99af4c54a9e0ed0e1d4338f2
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2021-07-14 00:24:46 +0530

    Added date to blog lists from meta-data

--- src/blog_lists.py
+++ src/blog_lists.py
@@ -9,64 +9,67 @@ TITLE_LABEL = "title"
 TAGS_LABEL = "tags"
 IMAGE_LABEL = "image"
 DESCRIPTION_LABEL = "description"
+DATE_LABEL = "date"
 
 articles_dir = "../blog/articles/"
 lists_dir = "../blog/list_pages/"
 
 def getMetaInfo(fileName):
-	with open(fileName, 'rb') as f:
-		htmlContent = f.read()
-	
-	soup = bs(htmlContent, 'html.parser')
-	meta_tags = soup.find_all('meta')
-	info = dict()
+    with open(fileName, 'rb') as f:
+        htmlContent = f.read()
 
-	for meta_tag in meta_tags:
-		info[meta_tag.get('name')] = meta_tag.get('content')
-		if meta_tag.get('name') == TAGS_LABEL:
-			info[meta_tag.get('name')] = [tag.strip() for tag in meta_tag.get('content').split(",")]
+    soup = bs(htmlContent, 'html.parser')
+    meta_tags = soup.find_all('meta')
+    info = dict()
 
-	return info
+    for meta_tag in meta_tags:
+        info[meta_tag.get('name')] = meta_tag.get('content')
+        if meta_tag.get('name') == TAGS_LABEL:
+            info[meta_tag.get('name')] = [tag.strip() for tag in meta_tag.get('content').split(",")]
+
+    return info
 
 def main():
-	all_articles = os.listdir(articles_dir)
+    all_articles = os.listdir(articles_dir)
 
-	# get meta information of all articles
-	all_meta = dict()
-	for article in all_articles:
-		meta = getMetaInfo(articles_dir + article)
-		all_meta[article] = meta
+    # get meta information of all articles
+    all_meta = dict()
+    for article in all_articles:
+        meta = getMetaInfo(articles_dir + article)
+        all_meta[article] = meta
 
-	# print(all_meta)
+    # print(all_meta)
 
-	# filter articles based on tags
-	filter_articles = {k:[] for k in TAGS}
-	for article in all_meta.keys():
-		for tag in all_meta[article][ TAGS_LABEL ]:
-			filter_articles[tag].append( article )
+    # filter articles based on tags
+    filter_articles = {k:[] for k in TAGS}
+    for article in all_meta.keys():
+        for tag in all_meta[article][ TAGS_LABEL ]:
+            filter_articles[tag].append( article )
 
-	print("==")
-	print(filter_articles)
+    print("==")
+    print(filter_articles)
 
-	# create HTML files for each of the blog lists according to tags
-	for tag in TAGS:
-		htmlContent = "\n"
-		for article in filter_articles[tag]:
-			htmlContent += "<div class='article'>\n"
-			htmlContent += "\t<h2><a href='" + articles_dir + article + "'>" + all_meta[article][ TITLE_LABEL ] + "</a></h2>\n"
-			htmlContent += "\t<p>" + all_meta[article][ DESCRIPTION_LABEL ] + "</p>\n"
-			htmlContent += "</div>\n"
-		with open(lists_dir + tag + ".html", 'w+') as f:
-			f.write(htmlContent)
+    # create HTML files for each of the blog lists according to tags
+    for tag in TAGS:
+        htmlContent = "\n"
+        for article in filter_articles[tag]:
+            htmlContent += "<div class='article'>\n"
+            htmlContent += "\t<h2><a href='" + articles_dir + article + "'>" + all_meta[article][ TITLE_LABEL ] + "</a></h2>\n"
+            htmlContent += "\t<small>" + all_meta[article].get( DATE_LABEL, '' ) + "</small>\n"
+            htmlContent += "\t<p>" + all_meta[article][ DESCRIPTION_LABEL ] + "</p>\n"
+            htmlContent += "</div>\n"
+        with open(lists_dir + tag + ".html", 'w+') as f:
+            f.write(htmlContent)
 
-	htmlContent = "\n"
-	for article in all_articles:
-		htmlContent += "<div class='article'>\n"
-		htmlContent += "\t<h2><a href='" + articles_dir + article + "'>" + all_meta[article][ TITLE_LABEL ] + "</a></h2>\n"
-		htmlContent += "\t<p>" + all_meta[article][ DESCRIPTION_LABEL ] + "</p>\n"
-		htmlContent += "</div>\n"
-	with open(lists_dir + "all.html", 'w+') as f:
-		f.write(htmlContent)
+    htmlContent = "\n"
+    for article in all_articles:
+        htmlContent += "<div class='article'>\n"
+        htmlContent += "\t<h2><a href='" + articles_dir + article + "'>" + all_meta[article][ TITLE_LABEL ] + "</a></h2>\n"
+        htmlContent += "\t<small>" + all_meta[article].get( DATE_LABEL, '' ) + "</small>\n"
+        htmlContent += "\t<p>" + all_meta[article][ DESCRIPTION_LABEL ] + "</p>\n"
+        htmlContent += "</div>\n"
+    with open(lists_dir + "all.html", 'w+') as f:
+        f.write(htmlContent)
 
 if __name__ == '__main__':
-	main()
+    main()
