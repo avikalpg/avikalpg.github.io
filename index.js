b2948a31606ceb35d416a77dd012144b83733cbd
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2025-01-08 12:34:32 +0530

    revamp: Completely changed the website from the ground up.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
 	// Start Google Analytics
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	(function (i, s, o, g, r, a, m) {
+		i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+			(i[r].q = i[r].q || []).push(arguments)
+		}, i[r].l = 1 * new Date(); a = s.createElement(o),
+			m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+	})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
 	ga('create', 'UA-78761399-1', 'auto');
 	ga('send', 'pageview');
@@ -12,67 +14,78 @@ $(document).ready(function(){
 
 	// console.log(ga.q);
 
-	$("div.wrapper div#main div#index").show();
-	$("div.wrapper div#main div#acads").hide();
-	$("div.wrapper div#main div#projects").hide();
-	$("div.wrapper div#main div#extra").hide();
-	$("div.wrapper div#main div#blog_content").hide();
+	$("div.wrapper div#main section#index").show();
+	$("div.wrapper div#main section#acads").hide();
+	$("div.wrapper div#main section#projects").hide();
+	$("div.wrapper div#main section#extra").hide();
+	$("div.wrapper div#main section#blog_content").hide();
 
-	$("div.wrapper div#main div#menu a.index").click(function(){
-		$("div.wrapper div#main div#picture").show();
-		$("div.wrapper div#main div#index").show();
-		$("div.wrapper div#main div#acads").hide();
-		$("div.wrapper div#main div#projects").hide();
-		$("div.wrapper div#main div#extra").hide();
-		$("div.wrapper div#main div#blog_content").hide();
+	$("div.wrapper div#menu a.index").click(function () {
+		$("div.wrapper div#main section#picture").show();
+		$("div.wrapper div#main section#index").show();
+		$("div.wrapper div#main section#acads").hide();
+		$("div.wrapper div#main section#projects").hide();
+		$("div.wrapper div#main section#extra").hide();
+		$("div.wrapper div#main section#blog_content").hide();
 
 		ga('create', 'UA-78761399-1', 'auto');
 		ga('send', 'pageview');
 	});
-	$("div.wrapper div#main div#menu a.acads").click(function(){
-		$("div.wrapper div#main div#picture").show();
-		$("div.wrapper div#main div#index").hide();
-		$("div.wrapper div#main div#acads").show();
-		$("div.wrapper div#main div#projects").hide();
-		$("div.wrapper div#main div#extra").hide();
-		$("div.wrapper div#main div#blog_content").hide();
+	$("div.wrapper div#menu a.acads").click(function () {
+		$("div.wrapper div#main section#picture").show();
+		$("div.wrapper div#main section#index").hide();
+		$("div.wrapper div#main section#acads").show();
+		$("div.wrapper div#main section#projects").hide();
+		$("div.wrapper div#main section#extra").hide();
+		$("div.wrapper div#main section#blog_content").hide();
 
 		ga('create', 'UA-78761399-1', 'auto', 'acadsTracker');
 		ga('acadsTracker.send', 'pageview');
 	});
-	$("div.wrapper div#main div#menu a.projects").click(function(){
-		$("div.wrapper div#main div#picture").show();
-		$("div.wrapper div#main div#index").hide();
-		$("div.wrapper div#main div#acads").hide();
-		$("div.wrapper div#main div#projects").show();
-		$("div.wrapper div#main div#extra").hide();
-		$("div.wrapper div#main div#blog_content").hide();
+	$("div.wrapper div#menu a.projects").click(function () {
+		$("div.wrapper div#main section#picture").show();
+		$("div.wrapper div#main section#index").hide();
+		$("div.wrapper div#main section#acads").hide();
+		$("div.wrapper div#main section#projects").show();
+		$("div.wrapper div#main section#extra").hide();
+		$("div.wrapper div#main section#blog_content").hide();
 
 		ga('create', 'UA-78761399-1', 'auto', 'projectsTracker');
 		ga('projectsTracker.send', 'pageview');
 	});
-	$("div.wrapper div#main div#menu a.extra").click(function(){
-		$("div.wrapper div#main div#picture").show();
-		$("div.wrapper div#main div#index").hide();
-		$("div.wrapper div#main div#acads").hide();
-		$("div.wrapper div#main div#projects").hide();
-		$("div.wrapper div#main div#extra").show();
-		$("div.wrapper div#main div#blog_content").hide();
+	$("div.wrapper div#menu a.extra").click(function () {
+		$("div.wrapper div#main section#picture").show();
+		$("div.wrapper div#main section#index").hide();
+		$("div.wrapper div#main section#acads").hide();
+		$("div.wrapper div#main section#projects").hide();
+		$("div.wrapper div#main section#extra").show();
+		$("div.wrapper div#main section#blog_content").hide();
 
 		ga('create', 'UA-78761399-1', 'auto', 'extraTracker');
 		ga('extraTracker.send', 'pageview');
 	});
-	$("div.wrapper div#main div#menu a.blog").click(function(){
-		$("div.wrapper div#main div#picture").hide();
-		$("div.wrapper div#main div#index").hide();
-		$("div.wrapper div#main div#acads").hide();
-		$("div.wrapper div#main div#projects").hide();
-		$("div.wrapper div#main div#extra").hide();
-		$("div.wrapper div#main div#blog_content").show();
-		$( "div.wrapper div#main div#blog_content iframe" ).attr( "src", function ( i, val ) { return val; });
+	$("div.wrapper div#menu a.blog").click(function () {
+		$("div.wrapper div#main section#picture").hide();
+		$("div.wrapper div#main section#index").hide();
+		$("div.wrapper div#main section#acads").hide();
+		$("div.wrapper div#main section#projects").hide();
+		$("div.wrapper div#main section#extra").hide();
+		$("div.wrapper div#main section#blog_content").show();
+		$("div.wrapper div#main section#blog_content iframe").attr("src", function (i, val) { return val; });
 
 		ga('create', 'UA-78761399-1', 'auto', 'blogTracker');
 		ga('blogTracker.send', 'pageview');
+	});
+
+	// Add modal functionality
+	document.addEventListener('DOMContentLoaded', function () {
+		// Add modal close functionality when clicking outside the modal
+		window.addEventListener('click', function (event) {
+			const modal = document.getElementById('contact-modal');
+			if (event.target === modal) {
+				modal.style.display = 'none';
+			}
+		});
 	});
 
 });
